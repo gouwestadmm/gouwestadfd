fa9af7ed625f3eecbedb8524450fa98999f7e8ce
gouwestadmm <lex@gouwestadmm.nl>
2016-10-11 16:04:56 +0200

    ss

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // general function for toggling stuff via data-attributes
 
 $('[data-action=body-action]').click(function(e) {
-    //e.preventDefault();
+    e.preventDefault();
     var bodyTarget = $(this).data("action-target");
     $('body').toggleClass(bodyTarget);
 
@@ -14,48 +14,15 @@ $('[data-action=body-action]').click(function(e) {
 });
 
 
+$(document).ready(function() {
+    
+    // load image url into parents background
+    $('.data-image').each(function() {
+        var imageUrl = $(this).find('img').attr('src');
+        $(this).css('background-image', 'url(' + imageUrl + ')');
+    });
 
-// CLOSE PAGE OVERLAY WHEN BACK BUTTON PRESSED
-// 
-
-function hashNav() {
-    // check if there is a url hash that contains *-active, and if so, 
-    // save it as a variable and prepend a 
-    // else, set variable as the default "all"
-    if(window.location.hash) {
-        console.log('contains hash');
-
-        var thehash = window.location.hash;
-           if (thehash *= "active"){
-
-              console.log(thehash); // returns "#search"
-              $('body').addClass(thehash);
-
-           }
-    }
-
-    //var filterOnLoad = window.location.hash ? (window.location.hash).replace('#', '') : '0';
-    //console.log(filterOnLoad);
-    //$('body').addClass(filterOnLoad);
-
-    // find object based on hash
-};
-
-/*
-if ("onhashchange" in window) {
-    console.log("The browser supports the hashchange event!");
-}
-
-function locationHashChanged() {
-    console.log('hash changed');
-    if (location.hash *= "active") {
-        var filterOnLoad = window.location.hash ? (window.location.hash).replace('#', '') : '0';
-        console.log(filterOnLoad);
-        $('body').addClass(filterOnLoad);
-    }
-}
-
-window.onhashchange = locationHashChanged();*/
+});
 
 
 //initialize swiper when document ready  
@@ -67,15 +34,3 @@ var homeProductSwiper = new Swiper('.home-product-swiper-container', {
     nextButton: '.swiper-next',
     prevButton: '.swiper-prev'
 })
-
-
-$(document).ready(function() {
-    
-    // load image url into parents background
-    $('.data-image').each(function() {
-        var imageUrl = $(this).find('img').attr('src');
-        $(this).css('background-image', 'url(' + imageUrl + ')');
-    });
-
-
-});
