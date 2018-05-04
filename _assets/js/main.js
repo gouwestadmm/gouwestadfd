921a10595984367624a6bc94f58d4fc9ab1ebdde
gouwestadmm <lex@gouwestadmm.nl>
2018-05-04 11:06:28 +0200

    updates random

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,17 +1,80 @@
-//
+
+
 // GENERAL TOGGLE FUNCTION
 // general function for toggling stuff via data-attributes
 
 $('[data-action=body-action]').click(function(e) {
-    //e.preventDefault();
+     e.preventDefault();
     var bodyTarget = $(this).data("action-target");
     $('body').toggleClass(bodyTarget);
-
-    if ($(this).data("sticky-action") === "rebuild") {
-        console.log('sticky rebuild started');
-        Stickyfill.rebuild();
-    };
 });
+
+//
+// NAV TOGGLE
+//
+
+$(".nav-toggle").click(function() {
+  $(".nav-toggle").toggleClass("cross");
+  $("body").toggleClass("navigation-active");
+
+  if( $("body").hasClass("menu-search-form-active")){
+    $("body").removeClass("menu-search-form-active");
+  }
+});
+
+$(".navbar-search-link").click(function() {
+  $(".nav-toggle").addClass("cross");
+  $("body").addClass("navigation-active");
+  $("body").addClass("menu-search-form-active");
+});
+
+$(".video-play-icon").click(function() {
+  var videoSrc = $(this).data("video-src");
+  $("#video-player").attr('src',videoSrc);
+});
+
+ $(".video-close-icon").click(function() {
+  $('#video-player').attr('src', $('#video-player').attr('src'));
+});
+
+$(".overlay-content-icon").click(function() {
+  var contentSrc = $(this).data("content-src");
+  $("#content-holder").attr('src',contentSrc);
+});
+
+ $(".overlay-content-close-icon").click(function() {
+  $('#content-holder').attr('src', $('#video-player').attr('src'));
+});
+
+
+//
+// SMOOTH SCROLLING
+//
+
+$(function() {
+    $('.smoothscroller').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 100
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
+// DATA IMAGE FUNCTION
+// general function for toggling stuff via data-attributes
+  $('.data-image').each(function(){
+    var imageUrl = $(this).find('img').attr('src');
+    $(this).css('background-image', 'url(' + imageUrl + ')');
+ });
+
+
 
 
 
@@ -69,13 +132,3 @@ var homeProductSwiper = new Swiper('.home-product-swiper-container', {
 })
 
 
-$(document).ready(function() {
-    
-    // load image url into parents background
-    $('.data-image').each(function() {
-        var imageUrl = $(this).find('img').attr('src');
-        $(this).css('background-image', 'url(' + imageUrl + ')');
-    });
-
-
-});
