1aa1d60ddac6ba58e6cd6e1a002f2c47465274ed
gouwestadmm <lex@gouwestadmm.nl>
2018-05-07 16:36:20 +0200

    updates

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -76,47 +76,6 @@ $(function() {
  });
 
 
-// CLOSE PAGE OVERLAY WHEN BACK BUTTON PRESSED
-// 
-
-function hashNav() {
-    // check if there is a url hash that contains *-active, and if so, 
-    // save it as a variable and prepend a 
-    // else, set variable as the default "all"
-    if(window.location.hash) {
-        console.log('contains hash');
-
-        var thehash = window.location.hash;
-           if (thehash *= "active"){
-
-              console.log(thehash); // returns "#search"
-              $('body').addClass(thehash);
-
-           }
-    }
-
-    var filterOnLoad = window.location.hash ? (window.location.hash).replace('#', '') : '0';
-    console.log(filterOnLoad);
-    $('body').addClass(filterOnLoad);
-    // find object based on hash
-};
-
-if ("onhashchange" in window) {
-    console.log("The browser supports the hashchange event!");
-}
-
-function locationHashChanged() {
-    console.log('hash changed');
-    if (location.hash *= "active") {
-        var filterOnLoad = window.location.hash ? (window.location.hash).replace('#', '') : '0';
-        console.log(filterOnLoad);
-        $('body').addClass(filterOnLoad);
-    }
-}
-
-window.onhashchange = locationHashChanged();
-
-
 //
 // CLOUDCANNON AJAX FORM DATA
 // Helper function to get form data in the supported format
