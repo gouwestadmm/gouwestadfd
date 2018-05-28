0c373c3f822d80d82fa5d304d810197ed5f33971
gouwestadmm <lex@gouwestadmm.nl>
2018-05-28 13:29:20 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -57,6 +57,16 @@ function locationHashChanged() {
 
 window.onhashchange = locationHashChanged();*/
 
+//initialize swiper when document ready  
+var defaultSwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    pagination: '.swiper-pagination',
+    slidesPerView: 'auto',
+    paginationClickable: true,
+    nextButton: '.swiper-next',
+    prevButton: '.swiper-prev'
+})
+
 
 //initialize swiper when document ready  
 var homeProductSwiper = new Swiper('.home-product-swiper-container', {
