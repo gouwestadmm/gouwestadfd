ae11b41306be4e849cad409a99785b2bb85a5b32
gouwestadmm <lex@gouwestadmm.nl>
2019-05-08 16:36:24 +0200

    aa

--- js/main.js
+++ js/main.js
@@ -83,55 +83,5 @@ $(document).ready(function() {
 
 
 
-function onSubmit(token) {
-   $.ajax({
-        type: 'post',
-        url: '#',
-        data: $('#bezichtiging-form').serialize(),
-        success: function () {
-            window.ConversationalForm.addRobotChatResponse("Bedankt voor je aanmelding.");
-            window.ConversationalForm.addRobotChatResponse("We nemen zo snel mogelijk contact met je op.");
-            var $el = $("#bezichtiging-confirmation");
-            setTimeout(function () {
-                $el.addClass('active');
-            }, 1000);
-            console.log("form send");
-        }
-    });
-}
 
-window.onload = function() {
-    var conversationalForm = window.cf.ConversationalForm.startTheConversation({
-        formEl: document.getElementById("bezichtiging-form"),
-        context: document.getElementById("cf-context"),
-        robotImage: "/img/logo-mark.svg",
-        submitCallback: function() {
-            grecaptcha.execute();
-        },
-        dictionaryData: {
-            "entry-not-found": "We hebben je invoer niet gevonde",
-            "input-placeholder": "Vul hier je antwoord in ...",
-            "input-placeholder-required": "Vul een antwoord in ...",
-            "input-placeholder-error": "Je invoer is onjuist ...",
-            "input-placeholder-file-error": "Upload mislukt ...",
-            "input-placeholder-file-size-error": "Helaas, je bestand is te groot ...",
-            "input-no-filter": "Geen resultaten voor {input-value}",
-            "user-reponse-and": " en ",
-            "user-reponse-missing": "Je hebt geen antwoord ingevuld of geen voorkeur ...",
-            "general": "Algemeen Type1|Algemeen Type2"
-        },
-        dictionaryRobot: {
-            "text": "Schrijf hier je antwoord.",
-            "input": "Schrijf hier je antwoord.",
-            "name": "Hoe heet je?",
-            "email": "Wat is je emailadres?",
-            "password": "Vul het wachtwoord in",
-            "tel": "Wat is je telefoonnummer?",
-            "radio": "Kies er 1.",
-            "checkbox": "Kies er zoveel als je wilt",
-            "select": "Kies een van deze mogelijkheden.",
-            "general": "Algemeen1|Algemeen2|Algemeen3.."
-        }// empty will throw error
-    });
-};
 
